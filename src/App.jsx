f809848a47ce6c45ec2e1be420be8ef6a11a0078
rk-tanvir790 <ktanvir790@gmail.com>
2026-03-09 07:39:52 +0600

    project setup, install Tailwind css, react icons, google font

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      {/* Header */}
+      <header>
+
+      </header>
+
+      {/* Main */}
+      <main>
+
+      </main>
+
+      {/* Footer */}
+      <footer>
+
+      </footer>
+    </>
+  )
+}
+
+export default App
