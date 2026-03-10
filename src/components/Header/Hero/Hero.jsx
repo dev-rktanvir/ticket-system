204096081951c5661bf56cb6beea48383bcef1f4
rk-tanvir790 <ktanvir790@gmail.com>
2026-03-10 07:37:09 +0600

    complete Hero section

---
+++ src/components/Header/Hero/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import StatusCard from '../../StatusCard/StatusCard';
+
+const Hero = () => {
+    return (
+        <div className='container mx-auto px-3 py-20 grid grid-cols-1 lg:grid-cols-2 gap-6'>
+            <StatusCard
+                title="In-Progress"
+                bgClr="bg-linear-to-r from-[#632EE3] to-[#9F62F2]"
+            >
+
+            </StatusCard>
+            <StatusCard
+                title="Resolved"
+                bgClr="bg-linear-to-r from-[#54CF68] to-[#00827A]"
+            >
+            </StatusCard>
+        </div>
+    );
+};
+
+export default Hero;
---
+++ src/components/StatusCard/StatusCard.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import bgImg1 from '../../assets/vector1.png';
+import bgImg2 from '../../assets/vector2.png';
+
+const StatusCard = ({ title, bgClr }) => {
+    return (
+        <div className={`relative overflow-hidden p-20 rounded-lg text-center ${bgClr}`}>
+            {/* Left Image */}
+            <img
+                src={bgImg1}
+                alt=""
+                className="absolute top-0 left-0 h-full"
+            />
+
+            {/* Right Image */}
+            <img
+                src={bgImg2}
+                alt=""
+                className="absolute top-0 right-0 h-full"
+            />
+            <div className='relative z-10'>
+                <h3 className='text-2xl text-white mb-4'>{title}</h3>
+                <span className='font-semibold text-6xl text-white'>0</span>
+            </div>
+
+        </div>
+    );
+};
+
+export default StatusCard;
